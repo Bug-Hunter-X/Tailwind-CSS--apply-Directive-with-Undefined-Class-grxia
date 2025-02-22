394d91bdeb1403727d195d6698ed07733329868a
BugHunter-X <askzenai@gmail.com>
2025-02-22 23:44:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,7 @@
+```javascript
+// Correct usage of @apply with a defined class
+// Ensure that the class 'bg-blue-500 p-4' is defined in your tailwind.config.js
+<div class="@apply bg-blue-500 p-4"></div>
+// Or use a utility directly
+<div class="bg-blue-500 p-4"></div>
+```
